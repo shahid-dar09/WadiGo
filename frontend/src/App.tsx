b5f207e90e90a367bccdacab6737601284742536
shahid ahmad dar <darshahid45ds@gmail.com>
2026-07-25 12:41:40 +0530

    fix: pass React Router v7 future flags to BrowserRouter to clean up console warnings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,7 +25,7 @@ export const App: React.FC = () => {
   }, [isDarkMode]);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <ScrollToTop />
       <PageTransitionLoader />
       <AppRoutes />
